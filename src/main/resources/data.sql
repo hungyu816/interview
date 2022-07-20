d67e806ee4b292c7c08aea1ba073e742841fa36e
hungyu816 <90588796+hungyu816@users.noreply.github.com>
2022-07-20 20:25:53 +0800

    Revert "v1.0"
    This reverts commit 6c1e7ae753369f45ce5ebf4990ace2b182d7774a.

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into coin values('USD','美金',30000);
-insert into coin values('GBP','英鎊',24000);
-insert into coin values('EUR','歐元',28000);
--- src/main/resources/schema.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE coin;
-
-create table coin (
-    coin_name varchar(100) primary key ,
-    chinese_name varchar(100) ,
-    rate float);
